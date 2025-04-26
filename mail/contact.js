28f0658ccfa46c9de435cf092bb98d577d615652
Me-saranv <sarasaran9597@gmail.com>
2025-04-26 18:34:55 +0530

    Update contact.js

--- mail/contact.js
+++ mail/contact.js
@@ -1,65 +1,53 @@
 $(function () {
-
     $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
-        submitError: function ($form, event, errors) {
-        },
+        submitError: function ($form, event, errors) {},
         submitSuccess: function ($form, event) {
             event.preventDefault();
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var subject = $("input#subject").val();
-            var message = $("textarea#message").val();
+            
+            let name = $("input#name").val();
+            let email = $("input#email").val();
+            let subject = $("input#subject").val();
+            let message = $("textarea#message").val();
+            let $this = $("#sendMessageButton"); // Corrected variable declaration
 
-            $this = $("#sendMessageButton");
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "contact.php",
+                url: "contact.php", // Ensure this file exists
                 type: "POST",
-                data: {
-                    name: name,
-                    email: email,
-                    subject: subject,
-                    message: message
-                },
+                data: { name, email, subject, message },
                 cache: false,
                 success: function () {
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                    $('#success > .alert-success')
-                            .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                            .append('</div>');
-                    $('#contactForm').trigger("reset");
+                    $("#success").html("<div class='alert alert-success'>")
+                        .find(".alert-success").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>")
+                        .append("<strong>Your message has been sent. </strong>")
+                        .append("</div>");
+                    $("#contactForm").trigger("reset");
                 },
                 error: function () {
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
-                    $('#success > .alert-danger').append('</div>');
-                    $('#contactForm').trigger("reset");
+                    $("#success").html("<div class='alert alert-danger'>")
+                        .find(".alert-danger").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>")
+                        .append($("<strong>").text("Sorry " + name + ", our mail server is not responding. Please try again later!"))
+                        .append("</div>");
+                    $("#contactForm").trigger("reset");
                 },
                 complete: function () {
-                    setTimeout(function () {
-                        $this.prop("disabled", false);
-                    }, 1000);
+                    setTimeout(() => { $this.prop("disabled", false); }, 1000);
                 }
             });
         },
         filter: function () {
             return $(this).is(":visible");
-        },
+        }
     });
 
-    $("a[data-toggle=\"tab\"]").click(function (e) {
+    $("a[data-toggle=\"tab\"]").on("click", function (e) {
         e.preventDefault();
         $(this).tab("show");
     });
 });
 
-$('#name').focus(function () {
-    $('#success').html('');
+$("#name").on("focus", function () {
+    $("#success").html("");
 });
